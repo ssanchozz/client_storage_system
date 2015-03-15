381dc22ee4d464d8560b32074eedb4e522ef0fb4
pushant <pushant@pushant>
2015-03-15 22:30:58 +0300

    Code review comments

--- ext_systems_support/ExtSystemsInterface.java
+++ ext_systems_support/ExtSystemsInterface.java
@@ -1,9 +1,11 @@
+//FIXME: same as for client entities, generally underscores look a bit scary here
 package ext_systems_support;
 
 import client_entities.Client;
 
 import java.util.ArrayList;
 
+//TODO: I know, we agreed on system1&2, may be call it ExtSystemsReader/Support?
 public interface ExtSystemsInterface {
 
     public ArrayList<Client> readData();
